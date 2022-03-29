d6ba1dfecc57eb0d47ffea994f54f695419080bc
fyupanquia <fyupanquia@outlook.com>
2022-03-29 16:45:52 -0500

    OOP

--- backend.py
+++ backend.py
@@ -1,52 +1,39 @@
 import sqlite3
 
-def connect():
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY, title TEXT, author INTEGER, year INTEGER, isbn INTEGER)")
-    conn.commit()
-    conn.close()
-    
-def insert(title,author,year,isbn):
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    cur.execute("INSERT INTO books VALUES(NULL,?,?,?,?)", (title,author,year,isbn))
-    conn.commit()
-    conn.close()
+class Database:
 
-def view():
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    cur.execute("SELECT * FROM books ORDER BY id DESC")
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    def __init__(self, db):
+        self.conn=sqlite3.connect(db)
+        self.cur=self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY, title TEXT, author INTEGER, year INTEGER, isbn INTEGER)")
+        self.conn.commit()
+        
+    def insert(self, title,author,year,isbn):
+        self.cur.execute("INSERT INTO books VALUES(NULL,?,?,?,?)", (title,author,year,isbn))
+        self.conn.commit()
 
-def search(title="",author="",year="",isbn=""):
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    cur.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    def view(self):
+        self.cur.execute("SELECT * FROM books ORDER BY id DESC")
+        return self.cur.fetchall()
 
-def delete(id):
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    cur.execute("DELETE FROM books WHERE id=?",(id,))
-    conn.commit()
-    conn.close()
+    def search(self, title="",author="",year="",isbn=""):
+        self.cur.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
+        return self.cur.fetchall()
 
-def update(id,title,author,year,isbn):
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    cur.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))
-    conn.commit()
-    conn.close()
+    def delete(self, id):
+        self.cur.execute("DELETE FROM books WHERE id=?",(id,))
+        self.conn.commit()
 
-connect()
-#insert("The sky","bruce",1001,32323233234)
-#update(1,"The Sun","william",2000,32323233232)
-#delete(1)
-print(view())
-#print(search(title="The sun"))
+    def update(self, id,title,author,year,isbn):
+        self.cur.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
+
+    #connect()
+    #insert("The sky","bruce",1001,32323233234)
+    #update(1,"The Sun","william",2000,32323233232)
+    #delete(1)
+    #print(view())
+    #print(search(title="The sun"))
--- frontend.py
+++ frontend.py
@@ -1,18 +1,20 @@
 from tkinter import *
-import backend
+from backend import Database
+
+database=Database("books.db")
 
 def view_command():
     list1.delete(0,END)
-    for row in backend.view():
+    for row in database.view():
         list1.insert(END,row)
 
 def search_command():
     list1.delete(0,END)
-    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+    for row in database.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
         list1.insert(END,row)
 
 def add_command():
-    backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    database.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
     view_command()
 
 
@@ -31,11 +33,11 @@ def get_selected_row(event):
         e4.insert(END,selected_tuple[4])
 
 def delete_command():
-    backend.delete(selected_tuple[0])
+    database.delete(selected_tuple[0])
     view_command()
 
 def update_command():
-    backend.update(selected_tuple[0], title_text.get(), author_text.get(),year_text.get(),isbn_text.get())
+    database.update(selected_tuple[0], title_text.get(), author_text.get(),year_text.get(),isbn_text.get())
     view_command()
 
 
